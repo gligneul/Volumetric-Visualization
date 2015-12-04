e2af20ffd78ab6a1e1ebe8ba001c2447712ed9de
Gabriel de Quadros Ligneul <gqligneul@gmail.com>
2015-12-04 20:22:23 -0200

    Added new results

--- main.c
+++ main.c
@@ -7,6 +7,7 @@
  * Trabalho Final: Visualização Volumétrica
  */
 
+#include <time.h>
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -18,8 +19,8 @@
 #define IMG_NX 128
 #define IMG_NY 99
 
-#define H 4.5
-#define THRESHOLD 0.6
+#define THRESHOLD 0.3
+#define DENSITY 0.05
 
 #define idx(i, j, k) ((k) * NY * NX + (j) * NX + (i))
 
@@ -27,42 +28,55 @@ typedef struct RayData {
     unsigned char *volume;
     int i;
     int k;
+    double h;
 } RayData;
 
 /* IO */
 static unsigned char *readctscan (const char *path);
-static unsigned char *visualize (unsigned char *volume);
 static int writepgm (const char *path, unsigned char *buffer, int nx, int ny);
+static void test_image (unsigned char *volume, unsigned char *bestimage,
+                        double h);
 
 /* Calculation */
+static unsigned char *visualize (unsigned char *volume, double h);
 static double getvalue (unsigned char *volume, int i, int k, double s);
-static double ray (unsigned char *volume, int i, int k);
+static double ray (unsigned char *volume, int i, int k, double h);
 static double volumetric_function (double s, void *data);
 static double transfer_function (double s, void *data);
-static double composite_simpson (double a, double b,
-        double (*f)(double x, void *data), void *data);
+static double composite_simpson (double start, double end, double h,
+                                 double (*f)(double x, void *data), void *data);
 static double simpson (double a, double b, double (*f)(double x, void *data),
                        void *data);
+static double mean_squared_error (unsigned char *a, unsigned char *b);
 
-int main () {
+int main ()
+{
     unsigned char *volume = readctscan("head-8bit.raw");
     if (!volume) {
         fputs("Unable to open the .raw file\n", stderr);
         return 1;
     }
 
-    unsigned char *image = visualize(volume);
-    free(volume);
+    unsigned char *image = visualize(volume, 4.5);
     if (writepgm("out.pgm", image, IMG_NX, IMG_NY)) {
         fputs("Unable to write the output image\n", stderr);
         return 1;
     }
-
     free(image);
+
+    printf("Mean squared error:\n");
+    printf("h\terror\n");
+    unsigned char *bestimage = visualize(volume, 1);
+    for (int i = 2; i < 20; ++i)
+        test_image(volume, bestimage, i);
+    free(bestimage);
+
+    free(volume);
     return 0;
 }
 
-static unsigned char *readctscan (const char *path) {
+static unsigned char *readctscan (const char *path)
+{
     FILE *file = fopen(path, "rb");
     if (!file)
         return NULL;
@@ -85,24 +99,8 @@ static unsigned char *readctscan (const char *path) {
     return NULL;
 }
 
-static unsigned char *visualize (unsigned char *volume) {
-    unsigned char *image = (unsigned char *)malloc(IMG_NX * IMG_NY);
-
-    for (int i = 0; i < IMG_NX; ++i) {
-        for (int j = 0; j < IMG_NY; ++j) {
-            double ray1 = ray(volume, 2 * i, j);
-            double ray2 = ray(volume, 2 * i + 1, j);
-            double value = (ray1 + ray2) / 2;
-            int index = i + IMG_NX * j;
-            printf("Ray: (%d, %d)\n", i, j);
-            image[index] = (unsigned char)(255 * value);
-        }
-    }
-
-    return image;
-}
-
-static int writepgm (const char *path, unsigned char *buffer, int nx, int ny) {
+static int writepgm (const char *path, unsigned char *buffer, int nx, int ny)
+{
     FILE *file = fopen(path, "w");
     if (!file)
         return 1;
@@ -118,11 +116,40 @@ static int writepgm (const char *path, unsigned char *buffer, int nx, int ny) {
     return 0;
 }
 
-static double getvalue (unsigned char *volume, int i, int k, double s) {
+static void test_image (unsigned char *volume, unsigned char *bestimage,
+                        double h)
+{
+    clock_t start = clock();
+    unsigned char *image = visualize(volume, h);
+    clock_t end = clock();
+    double time = (end - start)/(double)CLOCKS_PER_SEC;
+    printf("%g\t%g\t%g\n", h, mean_squared_error(bestimage, image), time);
+    free(image);
+}
+
+static unsigned char *visualize (unsigned char *volume, double h)
+{
+    unsigned char *image = (unsigned char *)malloc(IMG_NX * IMG_NY);
+
+    for (int i = 0; i < IMG_NX; ++i) {
+        for (int j = 0; j < IMG_NY; ++j) {
+            double ray1 = ray(volume, 2 * i, j, h);
+            double ray2 = ray(volume, 2 * i + 1, j, h);
+            double value = (ray1 + ray2) / 2;
+            int index = i + IMG_NX * j;
+            image[index] = (unsigned char)(255 * value);
+        }
+    }
+
+    return image;
+}
+
+static double getvalue (unsigned char *volume, int i, int k, double s)
+{
     int s1 = floor(s);
     int s2 = ceil(s);
 
-    if (s1 > NY || s2 > NY)
+    if (s1 >= NY || s2 >= NY)
         return volume[idx(i, NY - 1, k)] / 255.0;
 
     double v1 = volume[idx(i, s1, k)] / 255.0;
@@ -132,49 +159,68 @@ static double getvalue (unsigned char *volume, int i, int k, double s) {
     return v1 * (1 - alpha) + v2 * alpha;
 }
 
-static double ray (unsigned char *volume, int i, int k) {
-    RayData data = {volume, i, k};
-    double value = composite_simpson(0, NY - 1, volumetric_function, &data);
+static double ray (unsigned char *volume, int i, int k, double h)
+{
+    RayData data = {volume, i, k, h};
+    double value = composite_simpson(0, NY - 1, h, volumetric_function, &data);
     if (value > 1)
         return 1;
     else
         return value;
 }
 
-static double volumetric_function (double s, void *data) {
+static double volumetric_function (double s, void *data)
+{
+    RayData *raydata = (RayData *)data;
     return transfer_function(s, data)
-           * exp(-composite_simpson(0, s, transfer_function, data));
+           * exp(-composite_simpson(0, s, raydata->h, transfer_function, data));
 }
 
-static double transfer_function (double s, void *data) {
+static double transfer_function (double s, void *data)
+{
     RayData *ray = (RayData *)data;
-    double d = getvalue(ray->volume, ray->i, ray->k, s);
-    double threshold = THRESHOLD;
-    if (d < threshold)
+    double x = getvalue(ray->volume, ray->i, ray->k, s);
+    if (x < THRESHOLD)
         return 0;
     else
-        return 0.05 * (d - threshold);
+        return DENSITY * (x - THRESHOLD);
 }
 
-static double composite_simpson (double start, double end,
-        double (*f)(double x, void *data), void *data) {
+static double composite_simpson (double start, double end, double h,
+                                 double (*f)(double x, void *data), void *data)
+{
     double a = start;
-    double b = H;
+    double b = h;
     double sum = 0;
 
-    while (1) {
-        if (b > end)
-            return sum + simpson(a, end, f, data);
+    while (b < end) {
         sum += simpson(a, b, f, data);
         a = b;
-        b = b + H;
+        b = b + h;
     }
-    return 0;
+    if (a < end)
+        sum += simpson(a, end, f, data);
+    return sum;
 }
 
 static double simpson (double a, double b, double (*f)(double x, void *data),
-                       void *data) {
+                       void *data)
+{
     double h = b - a;
     return (h / 6) * (f(a, data) + 4 * f((a + b) / 2, data) + f(b, data));
+}
+
+static double mean_squared_error (unsigned char *a, unsigned char *b)
+{
+    double err = 0;
+    for (int i = 0; i < IMG_NX ; ++i) {
+        double lineerr = 0;
+        for (int j = 0; j < IMG_NY; ++j) {
+            double e = a[i + IMG_NX * j] - b[i + IMG_NX * j];
+            lineerr += e * e;
+        }
+        err += lineerr;
+    }
+    return err / IMG_NX * IMG_NY;
 }
 
